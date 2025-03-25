266275c35316fd8a18ecdac1815f3115ff07edcd
Alice Nilsson <alice.nilsson@vgregion.se>
2025-03-25 14:10:09 +0100

    Convert to Numpy before passing on to background worker

--- nnunetv2/inference/predict_from_raw_data.py
+++ nnunetv2/inference/predict_from_raw_data.py
@@ -370,7 +370,7 @@ class nnUNetPredictor(object):
                     sleep(0.1)
                     proceed = not check_workers_alive_and_busy(export_pool, worker_list, r, allowed_num_queued=2)
 
-                prediction = self.predict_logits_from_preprocessed_data(data).cpu()
+                prediction = self.predict_logits_from_preprocessed_data(data).cpu().numpy()
 
                 if ofile is not None:
                     # this needs to go into background processes
